1fe53e986f8dae814bd88c4e145962d8df641356
Charlie Li <charlieli@hotmail.com.tw>
2020-09-14 01:40:47 +0800

    run yarn lintfix

--- apollo/server/resolvers.ts
+++ apollo/server/resolvers.ts
@@ -4,19 +4,18 @@ const resolvers = {
       return [{ id: 0, firstName: "Nextjs" }];
     },
     tests: async (_, __, { dataSources }) => {
-      return await dataSources.testAPI.findTests();
-    }
+      return dataSources.testAPI.findTests();
+    },
   },
   Mutation: {
     insertTestMessage: async (_, { message }, { dataSources }) => {
       const test = await dataSources.testAPI.insertTestMessage({ message });
       if (!test) {
         return { success: false, message: "Something wrong happened" };
-      } else {
-        return { success: true, message: "success", test };
       }
-    }
-  }
+      return { success: true, message: "success", test };
+    },
+  },
 };
 
 export default resolvers;
