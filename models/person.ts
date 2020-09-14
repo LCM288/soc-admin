2109ef1230f91078d3295e8a0a22db80715215ce
Charlie Li <charlieli@hotmail.com.tw>
2020-09-15 02:08:48 +0800

    try try commit

--- models/person.ts
+++ models/person.ts
@@ -1,8 +1,12 @@
-export enum Gender {
-  male = "M",
-  female = "F",
+import { Sequelize, Model, DataTypes, Optional } from "sequelize";
+import { gql } from "apollo-server";
+
+enum Gender {
+  Male = "M",
+  Female = "F",
 }
-export enum College {
+
+enum College {
   CC = "CC",
   UC = "UC",
   NA = "NA",
@@ -14,16 +18,149 @@ export enum College {
   CW = "CW",
   None = "None",
 }
-export interface Person {
+
+// These are all the attributes in the Person model
+export interface PersonAttributes {
   id: number;
-  sid: number;
-  chineseName: string;
+  sid: string;
+  chineseName: string | null;
   englishName: string;
-  gender: Gender;
-  dateOfBirth?: Date;
-  email?: string;
-  phone?: string;
+  gender: Gender | null;
+  dateOfBirth: Date | null;
+  email: string | null;
+  phone: string | null;
   college: College;
-  major?: string;
-  expectedGrad: Date;
+  major: string;
+  dateOfEntry: Date;
+  expectedGraduationDate: Date;
 }
+
+// Some attributes are optional in `Person.build` and `Person.create` calls
+export type PersonCreationAttributes = Optional<PersonAttributes, "id">;
+
+export class Person
+  extends Model<PersonAttributes, PersonCreationAttributes>
+  implements PersonAttributes {
+  public id!: number;
+
+  public sid!: string;
+
+  public chineseName!: string | null;
+
+  public englishName!: string;
+
+  public gender!: Gender | null;
+
+  public dateOfBirth!: Date | null;
+
+  public email!: string | null;
+
+  public phone!: string | null;
+
+  public college!: College;
+
+  public major!: string;
+
+  public dateOfEntry!: Date;
+
+  public expectedGraduationDate!: Date;
+
+  // timestamps!
+  public readonly createdAt!: Date;
+
+  public readonly updatedAt!: Date;
+}
+
+export const PersonFactory = (sequelize: Sequelize): typeof Person => {
+  Person.init(
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      sid: {
+        type: DataTypes.STRING(12),
+        allowNull: false,
+        unique: true,
+      },
+      chineseName: {
+        type: DataTypes.STRING(20),
+      },
+      englishName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: DataTypes.ENUM,
+        values: Object.values(Gender),
+      },
+      dateOfBirth: {
+        type: DataTypes.DATE,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING(30),
+      },
+      college: {
+        type: DataTypes.ENUM,
+        values: Object.values(College),
+        allowNull: false,
+      },
+      major: {
+        type: DataTypes.STRING(8),
+        allowNull: false,
+      },
+      dateOfEntry: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      expectedGraduationDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "people",
+    }
+  );
+  return Person;
+};
+
+export const typeDefs = gql`
+  enum Gender {
+    Male
+    Female
+  }
+
+  enum College {
+    CC
+    UC
+    NA
+    SC
+    WS
+    WYS
+    SHHO
+    MC
+    CW
+    None
+  }
+
+  type Person {
+    id: ID!
+    sid: String!
+    chineseName: String
+    englishName: String!
+    gender: Gender
+    dateOfBirth: String
+    email: String
+    phone: String
+    college: College!
+    major: String!
+    dateOfEntry: String!
+    expectedGraduationDate: String!
+  }
+`;
