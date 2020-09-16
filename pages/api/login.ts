839a9d1f076a8a6446a9317cc96924d4e15f3605
Charlie Li <charlieli@hotmail.com.tw>
2020-09-17 01:28:33 +0800

    implemented sign in with microsoft

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,35 +1,71 @@
 import axios from "axios";
+import qs from "qs";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const TENANT = "link.cuhk.edu.hk";
-const CLIENT_ID = "373b4ec9-6336-4955-90cf-b7cbd9e3426f";
-const REDIRECT_URI = "http://localhost:3000/api/login";
-const CLIENT_SECRET = "vKst47W0fi_-I~qv9zP4utA~6IOhYeHXHm";
+const getAccessToken = async (
+  baseUrl: string,
+  code: string
+): Promise<string | undefined> => {
+  /* TODO put CLIENT_ID and CLIENT_SECRET to database */
+  const TENANT = "link.cuhk.edu.hk";
+  const CLIENT_ID = "373b4ec9-6336-4955-90cf-b7cbd9e3426f";
+  const CLIENT_SECRET = "vKst47W0fi_-I~qv9zP4utA~6IOhYeHXHm";
+  const REDIRECT_URI = `${baseUrl}/api/login`;
 
-export default async (req, res) => {
-  if (req.body.code) {
-    const link = `https://login.microsoftonline.com/${TENANT}/oauth2/v2.0/token`;
-    axios
-      .post(link, {
-        client_id: CLIENT_ID,
-        scope: "user.read",
-        code: req.body.code,
-        redirect_uri: REDIRECT_URI,
-        grant_type: "authorization_code",
-        client_secret: CLIENT_SECRET,
-      })
-      .then((res2) => {
-        console.log(`statusCode: ${res.statusCode}`);
-        // console.log(res2);
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-      .finally(() => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ name: "John Doe" }));
-      });
-  } else {
-    console.log("???");
+  const link = `https://login.microsoftonline.com/${TENANT}/oauth2/v2.0/token`;
+  const body = qs.stringify({
+    client_id: CLIENT_ID,
+    scope: "user.read",
+    code,
+    redirect_uri: REDIRECT_URI,
+    grant_type: "authorization_code",
+    client_secret: CLIENT_SECRET,
+  });
+
+  try {
+    const tokenResponse = await axios.post(link, body);
+    return tokenResponse.data.access_token;
+  } catch {
+    return undefined;
   }
+};
+
+const getSid = async (accessToken: string): Promise<string | undefined> => {
+  try {
+    const userDataResponse = await axios.get(
+      "https://graph.microsoft.com/v1.0/me",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+    const [sid] = userDataResponse.data.userPrincipalName.split("@");
+    return sid;
+  } catch {
+    return undefined;
+  }
+};
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (!req.body.code) {
+    res.status(400).end("No authorization code recieved.");
+  }
+  const { host } = req.headers;
+  const protocol = /^localhost/g.test(host) ? "http" : "https";
+  const baseUrl = `${protocol}://${req.headers.host}`;
+  const accessToken = await getAccessToken(baseUrl, req.body.code);
+
+  if (!accessToken) {
+    res.status(401).end("No access token recieved.");
+  }
+
+  const sid = await getSid(accessToken);
+
+  if (!sid) {
+    res.status(401).end("Cannot access user data.");
+  }
+
+  res.status(200).json({ sid });
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,38 @@
 import React from "react";
+import qs from "qs";
+import { GetServerSideProps } from "next";
 
-const TENANT = "link.cuhk.edu.hk";
-const CLIENT_ID = "373b4ec9-6336-4955-90cf-b7cbd9e3426f";
-const REDIRECT_URI = "http://localhost:3000/api/login";
+const microsoftLogin: React.FunctionComponent<{ baseUrl: string }> = ({
+  baseUrl,
+}) => {
+  /* TODO put CLIENT_ID to database */
+  const TENANT = "link.cuhk.edu.hk";
+  const CLIENT_ID = "373b4ec9-6336-4955-90cf-b7cbd9e3426f";
+  const REDIRECT_URI = `${baseUrl}/api/login`;
 
-const microsoftLogin: React.FunctionComponent = () => {
-  let link = "https://login.microsoftonline.com/";
-  link += `${TENANT}/oauth2/v2.0/authorize?`;
-  link += `client_id=${CLIENT_ID}`;
-  link += `&response_type=code`;
-  link += `&scope=user.read`;
-  link += `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
-  link += `&response_mode=form_post`;
-  link += `&prompt=select_account`;
-  link += `&domain_hint=${TENANT}`;
+  const body = qs.stringify({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    scope: "user.read",
+    redirect_uri: REDIRECT_URI,
+    response_mode: "form_post",
+    prompt: "select_account",
+    domain_hint: TENANT,
+  });
+
+  const link = `https://login.microsoftonline.com/${TENANT}/oauth2/v2.0/authorize?${body}`;
   return <a href={link}>login</a>;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { host } = req.headers;
+  const protocol = /^localhost/g.test(host) ? "http" : "https";
+  const baseUrl = `${protocol}://${req.headers.host}`;
+  return {
+    props: {
+      baseUrl,
+    },
+  };
 };
 
 export default microsoftLogin;
