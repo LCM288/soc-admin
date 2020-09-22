c2913a032edd4f9036efed5256794c73a40c0a2b
LCM288 <charlieli@hotmail.com.tw>
2020-09-22 21:12:47 +0800

    modify release.js

--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,12 @@
 /* eslint-disable */
 
+process.on("exit", function (code) {
+  return console.log(`About to exit with code ${code}`);
+});
+
 (async () => {
   const migration = require("../migrations/index.js");
   await migration();
   require("./create_jwt_secret.js");
+  process.exit(0);
 })();
