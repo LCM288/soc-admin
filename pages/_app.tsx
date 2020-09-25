60eaf8fb60f3f4d83b6a27a0de0801a3635d5d87
Anson Leung <ansonscleung@gmail.com>
2020-09-25 23:33:52 +0800

    Fix select arrow pop up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "bulma/bulma.sass";
+import "styles/select-fix.css";
 import { AppProps } from "next/app";
 import React from "react";
 import {
