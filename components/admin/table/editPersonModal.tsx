76b5207351b77d8c3fd84636c7f0b681436f4a0a
LCM288 <charlieli@hotmail.com.tw>
2020-10-26 18:45:40 +0800

    reset button

--- components/admin/table/editPersonModal.tsx
+++ components/admin/table/editPersonModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { College } from "@/models/College";
 import { Major } from "@/models/Major";
 import ChineseNameField from "components/register/chineseNameField";
@@ -32,9 +32,11 @@ const EditPersonModal: React.FunctionComponent<Props> = ({
   const [chineseName, setChineseName] = useState(
     (row.chineseName ?? "") as string
   );
-  const [gender, setGender] = useState((row.gender ?? "") as string);
+  const [gender, setGender] = useState((row.gender ?? "None") as string);
   const [dob, setDob] = useState((row.dateOfBirth ?? "") as string);
-  const [email, setEmail] = useState((row.email ?? "") as string);
+  const [email, setEmail] = useState(
+    (row.email ?? `${row.sid as string}@link.cuhk.edu.hk`) as string
+  );
   const [phone, setPhone] = useState((row.phone ?? "") as string);
   const [collegeCode, setCollegeCode] = useState(
     (row.college as College | undefined)?.code ?? ""
@@ -46,6 +48,30 @@ const EditPersonModal: React.FunctionComponent<Props> = ({
   const [doGrad, setDoGrad] = useState(
     (row.expectedGraduationDate ?? "") as string
   );
+  const onReset = useCallback(() => {
+    setEnglishName((row.englishName ?? "") as string);
+    setChineseName((row.chineseName ?? "") as string);
+    setGender((row.gender ?? "None") as string);
+    setDob((row.dateOfBirth ?? "") as string);
+    setEmail((row.email ?? `${row.sid as string}@link.cuhk.edu.hk`) as string);
+    setPhone((row.phone ?? "") as string);
+    setCollegeCode((row.college as College | undefined)?.code ?? "");
+    setMajorCode((row.major as Major | undefined)?.code ?? "");
+    setDoEntry((row.dateOfEntry ?? "") as string);
+    setDoGrad((row.expectedGraduationDate ?? "") as string);
+  }, [
+    row,
+    setEnglishName,
+    setChineseName,
+    setGender,
+    setDob,
+    setEmail,
+    setPhone,
+    setCollegeCode,
+    setMajorCode,
+    setDoEntry,
+    setDoGrad,
+  ]);
   return (
     <Modal show closeOnEsc={false} showClose={false} onClose={onCancel}>
       <Modal.Content className="has-background-white box">
@@ -73,10 +99,13 @@ const EditPersonModal: React.FunctionComponent<Props> = ({
           <DOGradField doGrad={doGrad} setDoGrad={setDoGrad} />
         </Section>
         <div className="is-pulled-right buttons">
-          <Button color="primary" onClick={() => onSave()}>
+          <Button type="button" onClick={onReset}>
+            Reset
+          </Button>
+          <Button type="button" color="primary" onClick={() => onSave()}>
             Confirm
           </Button>
-          <Button color="danger" onClick={onCancel}>
+          <Button type="button" color="danger" onClick={onCancel}>
             Cancel
           </Button>
         </div>
