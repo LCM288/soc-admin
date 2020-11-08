146c9fd8c9f91f60725e8c04dc977468cf253346
LCM288 <charlieli@hotmail.com.tw>
2020-11-08 14:03:38 +0800

    loginAs in dev mode

---
+++ pages/api/loginAs.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getClientIp } from "request-ip";
+import { issureJwt, setJwtHeader } from "utils/auth";
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (process.env.NODE_ENV !== "development") {
+    res.status(404).end();
+    return;
+  }
+  const { sid, name } = req.query;
+  if (typeof sid !== "string") {
+    res.status(400).end("sid not specified");
+    return;
+  }
+  if (typeof name !== "string") {
+    res.status(400).end("name not specified");
+    return;
+  }
+
+  const addr = getClientIp(req);
+  const user = { sid, name, addr };
+  const token = await issureJwt(user);
+
+  if (!token) {
+    res.status(500).end("Cannot find issue jwt.");
+    return;
+  }
+
+  setJwtHeader(token, res);
+  res.end("<script>window.location.href='/'</script>");
+};
