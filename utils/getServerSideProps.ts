0aabce04d09956eefc3b76e2e4e459099199c1eb
LCM288 <charlieli@hotmail.com.tw>
2020-11-10 22:20:49 +0800

    version bump for react & next

--- utils/getServerSideProps.ts
+++ utils/getServerSideProps.ts
@@ -2,13 +2,17 @@ import { User } from "@/types/datasources";
 import { GetServerSideProps } from "next";
 import { getUserAndRefreshToken, isAdmin } from "utils/auth";
 
+export interface ServerSideProps {
+  user: User | null;
+  isAdmin: boolean;
+}
+
 export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (
   ctx
 ) => {
   const user = await getUserAndRefreshToken(ctx);
   if (!user) {
-    ctx.res.statusCode = 307;
-    ctx.res.setHeader("Location", "/login");
+    return { redirect: { permanent: false, destination: "/login" } };
   }
   return {
     props: { user, isAdmin: await isAdmin(user) }, // will be passed to the page component as props
@@ -16,15 +20,11 @@ export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (
 };
 
 export const getAdminPageServerSideProps: GetServerSideProps = async (ctx) => {
-  const { props } = await getServerSideProps(ctx);
-  if (!props.isAdmin) {
-    ctx.res.statusCode = 401;
-    ctx.res.end("401 Unauthorized");
+  const { props } = (await getServerSideProps(ctx)) as {
+    props?: ServerSideProps;
+  };
+  if (!props?.isAdmin) {
+    return { notFound: true };
   }
   return { props };
 };
-
-export interface ServerSideProps {
-  user: User | null;
-  isAdmin: boolean;
-}
