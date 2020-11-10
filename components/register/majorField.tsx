27b252617ada513072dc6f51d200f3b86b5d3d9f
LCM288 <charlieli@hotmail.com.tw>
2020-11-10 23:19:40 +0800

    select field for major

--- components/register/majorField.tsx
+++ components/register/majorField.tsx
@@ -1,150 +1,119 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { useMemo } from "react";
-import { Form, Tag, Level } from "react-bulma-components";
-import ReactSelect from "react-select";
+import React, { useMemo, useCallback } from "react";
+import { Tag, Level } from "react-bulma-components";
 import { Major } from "@/models/Major";
 import { Faculty } from "@/models/Faculty";
 import { useQuery } from "@apollo/react-hooks";
 import toast from "utils/toast";
 import majorsQuery from "apollo/queries/major/majors.gql";
-
-const { Field, Label } = Form;
+import SelectField from "components/register/selectField";
 
 interface Props {
   majorCode: string;
   setMajorCode: (value: string) => void;
 }
 
-interface Labels {
-  value: string;
-  chineseLabel: string;
-  englishLabel: string;
-  faculties: { value: string; label: string }[];
-}
-
-const facultyColor: { [index: string]: string } = {
-  ART: "dark",
-  BAF: "info",
-  EDU: "danger",
-  ENF: "primary",
-  SLAW: "light",
-  MED: "success",
-  SCF: "warning",
-  SSF: "link",
-  DDP: "black",
-  IDM: "white",
-};
-
-const formatMajorOptionLabel = ({
-  chineseLabel,
-  englishLabel,
-  faculties,
-}: Labels) => (
-  <Level className="is-mobile react-select-major-label">
-    <Level.Side align="left">
-      <Level.Item>{englishLabel}</Level.Item>
-      <Level.Item>{chineseLabel}</Level.Item>
-    </Level.Side>
-    <Level.Side align="right">
-      {faculties.map((f) => (
-        <Level.Item key={f.value} className="has-tag">
-          <Tag
-            className="ml-2 has-text-weight-medium"
-            color={facultyColor[f.value]}
-          >
-            {f.label}
-          </Tag>
-        </Level.Item>
-      ))}
-    </Level.Side>
-  </Level>
-);
-
 const MajorField: React.FunctionComponent<Props> = ({
   majorCode,
   setMajorCode,
 }: Props) => {
   const majorsQueryResult = useQuery(majorsQuery);
-  const major = useMemo(() => {
-    const foundMajor = majorsQueryResult.data?.majors.find(
-      (m: Major) => m.code === majorCode
-    );
-    if (!foundMajor) {
-      return { value: "", chineseLabel: "", englishLabel: "", faculties: [] };
-    }
-    return {
-      value: foundMajor.code,
-      chineseLabel: foundMajor.chineseName,
-      englishLabel: foundMajor.englishName,
-      faculties: (foundMajor.faculties as Faculty[]).map((f: Faculty) => ({
-        value: f.code,
-        label: `${f.englishName} ${f.chineseName}`,
-      })),
-    };
-  }, [majorsQueryResult, majorCode]);
-  const majors = useMemo(() => {
-    return majorsQueryResult.data?.majors.map((majorProgram: Major) => ({
-      value: majorProgram.code,
-      chineseLabel: majorProgram.chineseName,
-      englishLabel: majorProgram.englishName,
-      faculties: (majorProgram.faculties as Faculty[]).map(
-        (faculty: Faculty) => ({
-          value: faculty.code,
-          label: `${faculty.englishName} ${faculty.chineseName}`,
-        })
-      ),
-    }));
-  }, [majorsQueryResult]);
+
+  interface MajorOption {
+    value: string;
+    chineseLabel: string;
+    englishLabel: string;
+    faculties: { value: string; label: string }[];
+  }
+
+  const majorOptions: MajorOption[] = useMemo(
+    () =>
+      majorsQueryResult.data?.majors.map((majorProgram: Major) => ({
+        value: majorProgram.code,
+        chineseLabel: majorProgram.chineseName,
+        englishLabel: majorProgram.englishName,
+        faculties: (majorProgram.faculties as Faculty[]).map(
+          (faculty: Faculty) => ({
+            value: faculty.code,
+            label: `${faculty.englishName} ${faculty.chineseName}`,
+          })
+        ),
+      })) ?? [],
+    [majorsQueryResult]
+  );
+
+  const selectedMajor = useMemo(
+    () =>
+      majorOptions.find(({ value }) => value === majorCode) ?? {
+        value: majorCode,
+        chineseLabel: majorCode,
+        englishLabel: majorCode,
+        faculties: [],
+      },
+    [majorOptions, majorCode]
+  );
+
+  const facultyColor = useMemo<{ [index: string]: string }>(
+    () => ({
+      ART: "dark",
+      BAF: "info",
+      EDU: "danger",
+      ENF: "primary",
+      SLAW: "light",
+      MED: "success",
+      SCF: "warning",
+      SSF: "link",
+      DDP: "black",
+      IDM: "white",
+    }),
+    []
+  );
+
+  const formatMajorOptionLabel = useCallback(
+    ({ chineseLabel, englishLabel, faculties }: MajorOption) => (
+      <Level className="is-mobile react-select-major-label">
+        <Level.Side align="left">
+          <Level.Item>{englishLabel}</Level.Item>
+          <Level.Item>{chineseLabel}</Level.Item>
+        </Level.Side>
+        <Level.Side align="right">
+          {faculties.map((faculty) => (
+            <Level.Item key={faculty.value} className="has-tag">
+              <Tag
+                className="ml-2 has-text-weight-medium"
+                color={facultyColor[faculty.value]}
+              >
+                {faculty.label}
+              </Tag>
+            </Level.Item>
+          ))}
+        </Level.Side>
+      </Level>
+    ),
+    [facultyColor]
+  );
+
+  const onChange = useCallback(
+    (input: MajorOption) => setMajorCode(input.value),
+    [setMajorCode]
+  );
+
   if (majorsQueryResult.error) {
     toast.danger(majorsQueryResult.error.message, {
       position: toast.POSITION.TOP_LEFT,
     });
   }
-  if (majorsQueryResult.loading) {
-    return <div>loading</div>;
-  }
 
   return (
-    <Field>
-      <Label>Major</Label>
-      <div>
-        <ReactSelect
-          value={major}
-          options={majors}
-          onChange={(input: Labels): void => {
-            setMajorCode(input.value);
-          }}
-          formatOptionLabel={formatMajorOptionLabel}
-          styles={{
-            input: (provided) => ({
-              ...provided,
-              top: "auto",
-              position: "absolute",
-            }),
-            valueContainer: (provided) => ({
-              ...provided,
-              position: "relative",
-              minHeight: "38px",
-            }),
-            singleValue: (provided) => ({
-              ...provided,
-              position: "relative",
-              top: "calc(50% + 2px)",
-              transform: "none",
-              whiteSpace: "normal",
-            }),
-          }}
-        />
-        <input
-          tabIndex={-1}
-          autoComplete="off"
-          className="hidden-input"
-          value={majorCode}
-          onChange={() => {}}
-          required
-        />
-      </div>
-    </Field>
+    <SelectField
+      label="Major"
+      selectedOption={selectedMajor}
+      options={majorOptions}
+      inputValue={majorCode}
+      onChange={onChange}
+      formatOptionLabel={formatMajorOptionLabel}
+      isLoading={majorsQueryResult.loading}
+      required
+    />
   );
 };
 
---
+++ components/register/selectField.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Form } from "react-bulma-components";
+import ReactSelect from "react-select";
+
+const { Field, Label } = Form;
+
+interface Props<SelectOption extends { value: string } = { value: string }> {
+  label: string;
+  selectedOption: SelectOption;
+  options: SelectOption[];
+  inputValue: string;
+  onChange: (newOption: SelectOption) => void;
+  formatOptionLabel: (option: SelectOption) => React.ReactNode;
+  defaultValue?: SelectOption | null;
+  isLoading?: boolean;
+  required?: boolean;
+}
+
+const SelectField = ({
+  label,
+  selectedOption,
+  options,
+  inputValue,
+  onChange,
+  formatOptionLabel,
+  defaultValue = null,
+  isLoading = false,
+  required = false,
+}: Props): React.ReactElement => {
+  return (
+    <Field>
+      <Label>{label}</Label>
+      <div>
+        <ReactSelect
+          defaultValue={defaultValue}
+          value={selectedOption}
+          options={options}
+          onChange={onChange}
+          formatOptionLabel={formatOptionLabel}
+          isLoading={isLoading}
+          styles={{
+            input: (provided) => ({
+              ...provided,
+              top: "auto",
+              position: "absolute",
+            }),
+            valueContainer: (provided) => ({
+              ...provided,
+              position: "relative",
+              minHeight: "38px",
+            }),
+            singleValue: (provided) => ({
+              ...provided,
+              position: "relative",
+              top: "calc(50% + 2px)",
+              transform: "none",
+              whiteSpace: "normal",
+            }),
+          }}
+        />
+        <input
+          tabIndex={-1}
+          autoComplete="off"
+          className="hidden-input"
+          value={inputValue}
+          onChange={() => {}}
+          required={required}
+        />
+      </div>
+    </Field>
+  );
+};
+
+SelectField.defaultProps = {
+  defaultValue: null,
+  isLoading: false,
+  required: false,
+};
+
+export default SelectField;
