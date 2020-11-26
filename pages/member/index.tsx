26ca7beaa511156401d5f3e54d5e5a708c1cfabf
LCM288 <charlielicmli@gmail.com>
2020-11-26 23:26:54 +0800

    executive setup page style update

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -160,18 +160,20 @@ const Index = ({ user, isAdmin }: ServerSideProps): React.ReactElement => {
 
   if (countExecutivesQueryResult.data?.countExecutives === 0) {
     return (
-      <IndexWrapper>
-        <>
-          <Head>
-            <title>Set youself as an executive</title>
-          </Head>
-          {isLoading && <Heading className="p-5 mb-0">Loading...</Heading>}
-          {socName && <Heading className="p-5 mb-0">{socName}</Heading>}
-          <div className="mb-5">{greeting}</div>
-          <Heading className="mb-5">Set youself as an executive.</Heading>
-          <ExecutiveSetup user={user} />
-        </>
-      </IndexWrapper>
+      <>
+        <IndexWrapper>
+          <>
+            <Head>
+              <title>Set youself as an executive</title>
+            </Head>
+            {isLoading && <Heading className="p-5 mb-0">Loading...</Heading>}
+            {socName && <Heading className="p-5 mb-0">{socName}</Heading>}
+            <div className="mb-5">{greeting}</div>
+          </>
+        </IndexWrapper>
+        <Heading className="mb-5">Set youself as an executive.</Heading>
+        <ExecutiveSetup user={user} />
+      </>
     );
   }
 
