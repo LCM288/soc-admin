cd9c48f223b1c5314948468eb6bf1486cea96ed7
LCM288 <charlielicmli@gmail.com>
2021-11-22 02:09:30 +0800

    cors support added

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,29 @@
 import apolloServer from "@/apolloServer";
+import microCors from "micro-cors";
+import process from "process";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default apolloServer.createHandler({ path: "/api/graphql" });
+const cors = microCors({
+  origin: process.env.CORS_ORIGINS ?? "*",
+  allowHeaders: [
+    "X-Requested-With",
+    "Access-Control-Allow-Origin",
+    "X-HTTP-Method-Override",
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Apollo-Query-Plan-Experimental",
+    "X-Apollo-Tracing",
+  ],
+});
+
+export default cors((req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+  return apolloServer.createHandler({ path: "/api/graphql" })(req, res);
+});
 
 export const config = {
   api: {
